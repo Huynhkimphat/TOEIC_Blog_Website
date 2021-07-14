bc4d1abb480064bbf524941ae8e32b05d68cb7fb
Xuân Trường <62050602+XuanTruong2408@users.noreply.github.com>
2021-07-14 22:33:20 +0700

    clean folder II

--- src/app/controllers/HomeController.js
+++
@@ -1,9 +0,0 @@
-class HomeController {
-    //* [GET]/
-    show(req, res, next) {
-        (async() => {
-            res.render("home");
-        })();
-    }
-}
-module.exports = new HomeController();
--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const homeRouter = require("./home");
-
-function route(app) {
-    // about status : 0 <=> not login , 1 <=> login with customer , 2 <=> login with staff , 3 <=> login with admin
-    // Route Home
-    app.use("/", homeRouter);
-}
-
-module.exports = route;
