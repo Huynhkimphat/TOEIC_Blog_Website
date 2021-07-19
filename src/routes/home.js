24b63e19efd389b650b24fff0ebf992611ccfddb
Phat Huynh <30569818+Huynhkimphat@users.noreply.github.com>
2021-07-19 10:41:12 +0700

    Delete src directory

--- src/routes/home.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const homeController = require("../app/controllers/HomeController");
-
-router.get("/", homeController.show);
-module.exports = router;
